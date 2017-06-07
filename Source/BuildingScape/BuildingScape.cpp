3af3d12516a33a2a53a8147c7c0285768c505f0a
Dani <daniandia@gmail.com>
2017-06-07 18:05:37 +0200

    First Submission project

---
+++ Source/BuildingScape/BuildingScape.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingScape.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BuildingScape, "BuildingScape" );
---
+++ Source/BuildingScape/BuildingScapeGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingScape.h"
+#include "BuildingScapeGameModeBase.h"
+
+
+
+
