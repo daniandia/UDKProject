f08e0ccaf0f5324b3c2142915da54be903b4b7d9
Dani Digipen <daniandia@gmail.com>
2017-06-08 10:27:20 +0200

    mix with dp computer

---
+++ Source/UdemiProject/UdemiProject.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UdemiProject.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UdemiProject, "UdemiProject" );
